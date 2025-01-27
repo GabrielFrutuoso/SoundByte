9b7f3045fdd9c7228832f4cd7880fad48e26be3f
GabrielFrutuoso <mackbiel03@gmail.com>
2025-01-27 16:09:44 -0300

    style: ajust input width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,12 +7,12 @@ export const Header = () => {
   return (
     <header className="flex items-center justify-between py-2 px-12 border-b border-zinc-800">
       <h1 className="text-2xl font-bold">SoundByte</h1>
-      <form className="w-2/4 relative">
+      <form className="w-96 relative">
         <Search
           size={20}
           className="absolute top-1/2 left-2 -translate-y-1/2"
         />
-        <Input className="bg-zinc-900 pl-8" type="text" />
+        <Input className="bg-zinc-900/50 pl-8" type="text" />
       </form>
       <Avatar>
         <AvatarFallback>CN</AvatarFallback>
