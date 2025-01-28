bcdae2950060c22c8e227c7c9dc7d556116fd78d
GabrielFrutuoso <mackbiel03@gmail.com>
2025-01-28 17:21:45 -0300

    style: ajust colors

--- src/components/RangeInput.tsx
+++ src/components/RangeInput.tsx
@@ -13,9 +13,9 @@ export function RangeInput({ className, ...props }: StyledRangeInputProps) {
         "w-full h-2 bg-secondary rounded-full appearance-none cursor-pointer",
         "focus:outline-none focus:ring-2 focus:ring-primary",
         "dark:bg-secondary dark:focus:ring-primary",
-        "[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-primary [&::-webkit-slider-thumb]:cursor-pointer",
-        "[&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-primary [&::-moz-range-thumb]:cursor-pointer [&::-moz-range-thumb]:border-none",
-        "dark:[&::-webkit-slider-thumb]:bg-primary dark:[&::-moz-range-thumb]:bg-primary",
+        "[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-lime-500 [&::-webkit-slider-thumb]:cursor-pointer",
+        "[&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-lime-500 [&::-moz-range-thumb]:cursor-pointer [&::-moz-range-thumb]:border-none",
+        "dark:[&::-webkit-slider-thumb]:bg-lime-500 dark:[&::-moz-range-thumb]:bg-lime-500",
         className,
       )}
       {...props}
