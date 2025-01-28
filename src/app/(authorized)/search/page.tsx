f56c6ccc183df88b5b67ac2458012cff01823691
GabrielFrutuoso <mackbiel03@gmail.com>
2025-01-28 18:01:16 -0300

    WIP: search page

--- src/app/(authorized)/search/page.tsx
+++ src/app/(authorized)/search/page.tsx
@@ -1,13 +1,40 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+"use client";
+
+import { SongItem } from "@/components/SongItem";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 export default function Search() {
+  const fakeArray = Array.from({ length: 36 }, (_, i) => i);
+  const searchParams = useSearchParams();
+
   return (
-    <div className="h-full">
-    <div className="flex gap-2 p-4 border-b border-zinc-800">
-        <Button variant="outline">Músicas</Button>
-        <Button variant="outline">Playlists</Button>
-        </div>    
+    <div className="h-full flex flex-col">
+      <div className="flex gap-2 p-4 border-b border-zinc-800">
+        <Button
+          variant={
+            searchParams.get("type") === "tracks" ? "default" : "outline"
+          }
+        >
+          Músicas
+        </Button>
+        <Button
+          variant={
+            searchParams.get("type") === "playlists" ? "default" : "outline"
+          }
+        >
+          Playlists
+        </Button>
+      </div>
+      <ScrollArea>
+        <div className="flex flex-wrap gap-6 items-center justify-center p-6">
+          {fakeArray.map((_, i) => (
+            <SongItem key={i + "teste"} />
+          ))}
+        </div>
+      </ScrollArea>
     </div>
-  )
+  );
 }
