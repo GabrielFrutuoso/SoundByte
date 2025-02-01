d0f06ccc2739aa1ec0b172d71947da42de22f11f
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-01 13:24:05 -0300

    feat: add file feature

--- src/components/FileInput/index.tsx
+++ src/components/FileInput/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { Music4 } from "lucide-react";
 
-export const FileInput = (props: React.ComponentProps<typeof Input>) => {
+interface Props extends React.ComponentProps<typeof Input> {
+  children?: React.ReactNode;
+}
+
+export const FileInput = ({ children, ...props }: Props) => {
   return (
     <>
       <Label
-        className="aspect-square w-56 cursor-pointer rounded-md flex items-center justify-center border border-dashed border-zinc-800/50 hover:bg-zinc-800/50"
+        className="aspect-square w-56 cursor-pointer rounded-lg flex items-center justify-center border border-zinc-800/50 hover:bg-zinc-800/50"
         htmlFor={props.id}
       >
-        <Music4 size={40} />
+        {children}
       </Label>
       <Input id={props.id} type="file" className="hidden" {...props} />
     </>
