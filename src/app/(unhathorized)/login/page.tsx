b0ce9c1b9ab4dca7ae98c9e2a4a7c4daacb55d6c
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-03 11:57:07 -0300

    feat: add credentials auth

--- src/app/(unhathorized)/login/page.tsx
+++ src/app/(unhathorized)/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { LoginButton } from "@/components/LoginButton";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
@@ -6,13 +8,16 @@ import { Label } from "@/components/ui/label";
 import { RetroGrid } from "@/components/ui/retro-grid";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
-import { Metadata } from "next";
+import { signIn } from "next-auth/react";
 
-export const metadata: Metadata = {
-  title: "Login",
-  openGraph: {
-    title: "SoundByte - Login"
-  }
+const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  signIn("credentials", {
+    callbackUrl: "/",
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
 };
 
 export default function Login() {
@@ -24,18 +29,23 @@ export default function Login() {
           <h2 className="w-full text-center text-2xl font-bold">Login</h2>
         </CardHeader>
         <CardContent>
-          <form action="" className="flex flex-col space-y-4">
+          <form className="flex flex-col space-y-4" onSubmit={onSubmit}>
             <div className="flex flex-col space-y-1">
               <Label htmlFor="email" className="text-sm">
                 Email
               </Label>
-              <Input type="email" id="email" className="input" />
+              <Input type="email" id="email" name="email" className="input" />
             </div>
             <div className="flex flex-col space-y-1">
               <Label htmlFor="password" className="text-sm">
                 Password
               </Label>
-              <Input type="password" id="password" className="input" />
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                className="input"
+              />
             </div>
             <span className="text-right">
               Já tem uma conta? &nbsp;
