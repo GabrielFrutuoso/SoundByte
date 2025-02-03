20efea4776655abec909a2e732a2f24549697ac8
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-03 14:55:38 -0300

    feat: add tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,19 @@
 import { Toaster } from "@/components/ui/toaster";
 import { SessionProvider } from "@/providers/SessionProvider";
 import { NuqsAdapter } from "nuqs/adapters/next";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: true,
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+    },
+  },
+});
 
 export default function RootLayout({
   children,
@@ -14,8 +27,10 @@ export default function RootLayout({
       <body className="dark h-screen w-full flex flex-col">
         <SessionProvider>
           <NuqsAdapter>
+          <QueryClientProvider client={queryClient}>
             {children}
             <Toaster />
+          </QueryClientProvider>
           </NuqsAdapter>
         </SessionProvider>
       </body>
