ca97351487aa5b49a741f099531f9ec6446e5997
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-03 13:39:44 -0300

    feat: add register feature

--- src/app/(unhathorized)/register/page.tsx
+++ src/app/(unhathorized)/register/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { LoginButton } from "@/components/LoginButton";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
@@ -5,16 +7,38 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RetroGrid } from "@/components/ui/retro-grid";
 import { Separator } from "@/components/ui/separator";
+import { useToast } from "@/hooks/use-toast";
+import { apiClient } from "@/lib/apiClient";
 import Link from "next/link";
-
-export const metadata = {
-  title: "Register",
-  openGraph: {
-    title: "SoundByte - Register"
-  }
-};
+import { useRouter } from "next/navigation";
 
 export default function Register() {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    try {
+      const res = await apiClient.post("api/user/register", {
+        email: formData.get("email"),
+        password: formData.get("password"),
+        username: formData.get("username"),
+      });
+
+      if (res.status === 200) {
+        router.push("/login");
+      }
+    } catch (error) {
+      console.error("Error in POST /api/user/register:", error);
+      toast({
+        title: "Error",
+        description: "Failed to register user",
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen gap-3">
       <Card className="z-20 w-80 md:w-96">
@@ -23,24 +47,34 @@ export default function Register() {
           <h2 className="w-full text-center text-2xl font-bold">Registrar</h2>
         </CardHeader>
         <CardContent>
-          <form action="" className="flex flex-col space-y-4">
+          <form onSubmit={onSubmit} className="flex flex-col space-y-4">
             <div className="flex flex-col space-y-1">
-              <Label htmlFor="nick" className="text-sm">
+              <Label htmlFor="username" className="text-sm">
                 Nome de usuário
               </Label>
-              <Input type="text" id="nick" className="input" />
+              <Input
+                type="text"
+                id="username"
+                name="username"
+                className="input"
+              />
             </div>
             <div className="flex flex-col space-y-1">
               <Label htmlFor="email" className="text-sm">
                 Email
               </Label>
-              <Input type="email" id="email" className="input" />
+              <Input type="email" id="email" name="email" className="input" />
             </div>
             <div className="flex flex-col space-y-1">
               <Label htmlFor="password" className="text-sm">
                 Password
               </Label>
-              <Input type="password" id="password" className="input" />
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                className="input"
+              />
             </div>
             <span className="text-right">
               Não tem uma conta? &nbsp;
