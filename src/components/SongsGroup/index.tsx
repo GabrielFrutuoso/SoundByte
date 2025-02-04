1d27411a3b93b6bfe0862acd4e3a64b6affc4b8c
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-04 12:52:27 -0300

    feat: create a song group component

---
+++ src/components/SongsGroup/index.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import React from "react";
+import { SongItem } from "../SongItem";
+import { SongItemProps } from "@/app/types/SongProps.type";
+import { useGetSongs } from "@/hooks/requests/useGetSongs.ts";
+
+interface SongGroupProps {
+    title: string;
+    description: string;
+    redirectTo: string
+}
+
+export const SongGroup = ({ title, description, redirectTo }: SongGroupProps) => {
+  const { data, isLoading, error } = useGetSongs("", "", 6);
+
+  console.log(data, isLoading, error);
+
+  return (
+    <div className="flex flex-col gap-2 mt-12">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col">
+          <h2 className="text-xl font-bold">{title}</h2> 
+          <p className="text-sm font-thin opacity-75">{description}</p> 
+        </div>
+        
+        <Link className="underline" href={redirectTo}>
+          Ver todas
+        </Link>
+      </div>
+      <ul className="grid grid-cols-6 gap-1">
+        {data?.data?.map((song: SongItemProps) => (
+          <SongItem key={song?.id} {...song} />
+        ))}
+      </ul>
+    </div>
+  );
+};
