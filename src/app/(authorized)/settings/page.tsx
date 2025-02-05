171ddc4d9924c1ca374da4cf9d83877f1f6b5fd3
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-05 19:22:48 -0300

    WIP: settings page

--- src/app/(authorized)/settings/page.tsx
+++ src/app/(authorized)/settings/page.tsx
@@ -1,26 +1,41 @@
-'use client'
+"use client";
 
-import { Avatar, AvatarFallback } from '@/components/ui/avatar'
-import { Separator } from '@/components/ui/separator'
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { useUserStore } from "@/store/userStore";
+import React from "react";
+import { redirect } from "next/navigation";
 
-export default function Settings(){
+export default function Settings() {
+  const { user } = useUserStore();
+
+  if (!user) {
+    return redirect("/");
+  }
+
   return (
-    <div className='h-full flex flex-col p-12'>
-        <h1 className='text-2xl font-bold'>Settings</h1>
+    <div className="h-full flex flex-col p-12">
+      <h1 className="text-2xl font-bold">Settings</h1>
 
-        <Separator orientation='horizontal' />
+      <Separator className="my-6" orientation="horizontal" />
 
-        <div className='flex gap-4'>
-            <Avatar>
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <div className='flex flex-col gap-4'>
-              <h3>Gabriel Frutuoso</h3>
-              <p>gabrielfrutuoso@gmail.com</p>
-            </div>
+      <div className="flex items-center justify-start gap-4">
+        <Avatar className="w-40 h-40">
+          <AvatarImage
+            alt="Avatar"
+            src={user?.avatar || ""}
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col items-start justify-start gap-2">
+          <h3 className="text-4xl font-bold">{user?.username}</h3>
+          <p className="text-lg">{user?.email}</p>
+          <Button variant="outline"><Pencil /> Editar perfil</Button>
         </div>
+      </div>
 
     </div>
-  )
+  );
 }
