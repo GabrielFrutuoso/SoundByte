1072b24656d81fe03ab89df8642d8edca1428c95
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-06 16:14:03 -0300

    feat? add song

--- src/forms/AddSongForm/index.tsx
+++ src/forms/AddSongForm/index.tsx
@@ -14,14 +14,46 @@ import {
 import Image from "next/image";
 import { FileInput } from "@/components/FileInput";
 import { ImageDownIcon } from "lucide-react";
+import { usePostSong } from "@/hooks/requests/usePostSong";
+import { useUserStore } from "@/store/userStore";
+import { toast } from "@/hooks/use-toast";
+import { Button } from "@/components/ui/button";
 
 export const AddSongForm = () => {
   const [fileMusic, setMusicFile] = React.useState<File | null>(null);
   const [fileBanner, setBannerFile] = React.useState<File | null>(null);
   console.log(fileMusic?.name);
 
+  const { mutate } = usePostSong();
+  const { user } = useUserStore();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (fileMusic) {
+        mutate({
+          userUUID: user?.id as string,
+          title: e.currentTarget.title.value as string,
+          artist: e.currentTarget.artist.value,
+          isPrivate: e.currentTarget.privacy.value === "private" ? true : false,
+          songURL: "test",
+          bannerSrc:
+            "https://i.scdn.co/image/ab67616d0000b273df51a3d66223e5b01813e0c4",
+          genreId: 1,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: "Erro",
+        description: "Ocorreu um erro ao publicar a musica",
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
-    <form className="flex items-center gap-6">
+    <form className="flex items-center gap-6" onSubmit={onSubmit}>
       <div className="flex flex-col items-center gap-6 flex-1">
         <FormField>
           <Label htmlFor="title">Titulo</Label>
@@ -52,6 +84,9 @@ export const AddSongForm = () => {
             onChange={(e) => setMusicFile(e.target.files?.[0] || null)}
           />
         </FormField>
+        <Button className="w-full" type="submit">
+          Publicar
+        </Button>
       </div>
       {fileBanner ? (
         <Image
