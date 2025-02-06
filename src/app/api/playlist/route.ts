d1c58907764b4ce47cb1dbf42346e8cc0b0c7a3c
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-06 18:15:22 -0300

    feat: add get playlist route

---
+++ src/app/api/playlist/route.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/PrismaClient";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const playlists = await prisma.playlist.findMany({
+      select: {
+        id: true,
+        title: true,
+        bannerSrc: true,
+      },
+      where: {
+        isPrivate: false
+      },
+
+    });
+
+    return NextResponse.json(playlists, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching public playlists:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch public playlists" },
+      { status: 500 }
+    );
+  }
+}
