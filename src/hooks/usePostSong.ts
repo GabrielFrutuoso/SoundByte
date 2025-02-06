9eb9b0d0e2fbfe627feb55faef8590ea553fffd5
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-06 15:12:30 -0300

    feat: add post song hook

---
+++ src/hooks/usePostSong.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+
+export interface ISongPost {
+  userUUID: string;
+  title: string;
+  artist: string;
+  bannerSrc: string;
+  songURL: string;
+  genreId: number;
+  isPrivate: boolean;
+}
+
+const postSong = async (songData: ISongPost) => {
+  const response = await axios.post('http://localhost:3000/api/songs', songData);
+  return response.data;
+};
+
+export const usePostSong = () => {
+  const queryClient = useQueryClient();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return useMutation<any, Error, ISongPost>({
+    mutationFn: postSong,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['songs'] });
+    },
+  });
+};
