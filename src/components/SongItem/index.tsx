e5534883c8cc168266600cfeda8e0a84e3d24bdc
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-09 15:05:55 -0300

    feat: able to select a single song

--- src/components/SongItem/index.tsx
+++ src/components/SongItem/index.tsx
@@ -2,10 +2,20 @@ import { SongItemProps } from "@/app/types/SongProps.type";
 import { Play } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import { usePlaylistStore } from "@/store/playlistStore";
 
 export const SongItem = (song: SongItemProps) => {
+  const { singleSongId, setPlaylistId, setSingleSongId, setIndex } =
+    usePlaylistStore();
+
+    const handleSongSelect = (id: string | number) => {
+      setPlaylistId('');
+      setSingleSongId(String(id));
+      setIndex(0);
+    };
+    
   return (
-    <div className="flex flex-col gap-2 w-60 cursor-pointer group">
+    <div onClick={() => handleSongSelect(song.id)} className={`flex flex-col gap-2 w-60 cursor-pointer group ${singleSongId === song.id ? 'text-lime-500' : ''}`}>
       <div className="relative">
         <button className="flex items-center justify-center bg-white/25 group-hover:visible invisible absolute w-full h-full rounded-lg">
           <Play size={40} />
