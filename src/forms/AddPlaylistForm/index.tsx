b7683dfbcbfdb139f9038afcffad4f27a3618d23
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-10 11:14:52 -0300

    feat: add post playlist feature

--- src/forms/AddPlaylistForm/index.tsx
+++ src/forms/AddPlaylistForm/index.tsx
@@ -14,12 +14,41 @@ import {
 import Image from "next/image";
 import { FileInput } from "@/components/FileInput";
 import { ImageDownIcon } from "lucide-react";
+import { useUserStore } from "@/store/userStore";
+import { toast } from "@/hooks/use-toast";
+import { usePostPlaylist } from "@/hooks/requests/playlist/usePostPlaylist";
+import { Button } from "@/components/ui/button";
 
 export const AddPlaylistForm = () => {
   const [fileBanner, setBannerFile] = React.useState<File | null>(null);
 
+  const { mutate } = usePostPlaylist();
+  const { user } = useUserStore();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (fileBanner) {
+        mutate({
+          userUUID: user?.id as string,
+          title: e.currentTarget.title.value as string,
+          isPrivate: e.currentTarget.privacy.value === "private" ? true : false,
+          bannerSrc:
+            "https://preview.redd.it/meteora-20-tracklist-released-on-genius-the-photo-below-was-v0-3mz7u1olpkea1.jpg?width=774&format=pjpg&auto=webp&s=8581bad06e7368b4556404ac496a543935daa873",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: "Erro",
+        description: "Ocorreu um erro ao publicar a musica",
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
-    <form className="flex items-center gap-6">
+    <form onSubmit={onSubmit} className="flex items-center gap-6">
       <div className="flex flex-col items-center gap-6 flex-1">
         <FormField>
           <Label htmlFor="title">Titulo</Label>
@@ -37,6 +66,7 @@ export const AddPlaylistForm = () => {
             </SelectContent>
           </Select>
         </FormField>
+        <Button type="submit">Publicar</Button>
       </div>
       {fileBanner ? (
         <Image
