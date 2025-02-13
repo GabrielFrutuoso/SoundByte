6db686975ef2848c7a7a8096899066ef145550f3
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-13 16:07:55 -0300

    feat: ajust component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,15 +16,19 @@ import {
   MenubarMenu,
   MenubarTrigger,
 } from "../ui/menubar";
-import { useQueryState } from "nuqs";
 import { useUserStore } from "@/store/userStore";
 import Image from "next/image";
 
 export const Header = () => {
   const router = useRouter();
-  const [query, setQuery] = useQueryState("query");
   const { user } = useUserStore();
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const query = formData.get("query");
+    router.push(`/search?query=${query}`);
+  };
   return (
     <header className="flex items-center justify-between py-2 px-12 border-b border-zinc-800">
       <Link href={"/"}>
@@ -33,15 +37,17 @@ export const Header = () => {
         </h1>
       </Link>
       <div className="flex items-center gap-4">
-        <form className="w-96 relative">
+        <form onSubmit={handleSubmit} className="w-96 relative">
           <Search
             size={20}
             className="absolute top-1/2 left-2 -translate-y-1/2"
           />
           <Input
-            onChange={(e) => setQuery(e.target.value)}
             className="bg-zinc-900/50 pl-8"
             type="text"
+            placeholder="Pesquisar"
+            name="query"
+            autoComplete="off"
           />
         </form>
         {user?.username && <AddSongDialog />}
