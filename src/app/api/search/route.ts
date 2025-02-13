0bd44d1c9e4c071abfa66cd74186b2ccd275b2d4
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-13 16:05:09 -0300

    feat: add search param type and ajust the api response

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -5,83 +5,59 @@ export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     const query = searchParams.get("q") || "";
+    const type = searchParams.get("type") || "";
 
-    const [songs, playlists] = await Promise.all([
-      prisma.song.findMany({
-        where: {
-          isPrivate: false,
-          OR: [
-            {
-              title: {
-                contains: query,
-              },
-            },
-            {
-              artist: {
-                contains: query,
-              },
-            },
-            {
-              genre: {
+    switch (type) {
+      case "song":
+        const songs = await prisma.song.findMany({
+          where: {
+            isPrivate: false,
+            OR: [
+              {
                 title: {
                   contains: query,
                 },
               },
-            },
-          ],
-        },
-        include: {
-          user: {
-            select: {
-              id: true,
-              username: true,
-              avatar: true,
-            },
-          },
-          genre: {
-            select: {
-              title: true,
-            },
-          },
-        },
-      }),
-      prisma.playlist.findMany({
-        where: {
-          isPrivate: false,
-          OR: [
-            {
-              title: {
-                contains: query,
-              },
-            },
-          ],
-        },
-        include: {
-          user: {
-            select: {
-              id: true,
-              username: true,
-              avatar: true,
-            },
-          },
-          songs: {
-            include: {
-              song: {
-                include: {
-                  user: true,
-                  genre: true,
+              {
+                artist: {
+                  contains: query,
                 },
               },
-            },
+              {
+                genre: {
+                  title: {
+                    contains: query,
+                  },
+                },
+              },
+            ],
           },
-        },
-      }),
-    ]);
+          select: {
+            id: true,
+            title: true,
+            bannerSrc: true,
+            artist: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        });
 
-    return NextResponse.json({
-      songs,
-      playlists,
-    });
+        return NextResponse.json(songs, { status: 200 });
+      case "playlist":
+        const playlists = await prisma.playlist.findMany({
+          where: {
+            isPrivate: false,
+            OR: [
+              {
+                title: {
+                  contains: query,
+                },
+              },
+            ],
+          },
+        });
+        return NextResponse.json(playlists, { status: 200 });
+    }
   } catch (err) {
     console.error(err);
     return NextResponse.json(
