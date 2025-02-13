91508a48dfea153347e281fa8c35826db9e87da7
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-13 17:58:41 -0300

    feat: implements pagination

--- src/app/(authorized)/search/page.tsx
+++ src/app/(authorized)/search/page.tsx
@@ -1,68 +1,81 @@
 "use client";
 
-import { ScrollArea } from "@/components/ui/scroll-area";
 import React, { useEffect, useState } from "react";
 import { useQueryState } from "nuqs";
 import { Button } from "@/components/ui/button";
 import { useSearch } from "@/hooks/requests/search/useSearch";
 import { SearchResultItem } from "@/components/SearchResultItem";
 import { SearchResultType } from "@/app/types/SearchResult.type";
+import { Separator } from "@/components/ui/separator";
+import { Pagination } from "@/components/Pagination";
 
 export default function Search() {
   const [type, setType] = useState("song");
   const [query] = useQueryState("query");
+  const [page, setPage] = useQueryState("page");
 
   useEffect(() => {
     setType("song");
   }, []);
 
-  const { data: result } = useSearch(query || "", type || "");
+  const { data: result } = useSearch(
+    query || "",
+    type || "song",
+    Number(page || 1),
+    12
+  );
 
   console.log(result?.data);
 
-  return (
-    <div className="h-full flex flex-col">
-      <div className="flex gap-2 p-4 border-b border-zinc-800">
-        <Button
-          onClick={() => setType("song")}
-          variant={type === "song" ? "default" : "ghost"}
-          size="default"
-        >
-          Músicas
-        </Button>
+  // Wrap setPage to convert its type
+  const handlePageChange = (newPage: number) => {
+    setPage(String(newPage));
+  };
 
-        <Button
-          onClick={() => setType("playlist")}
-          variant={type === "playlist" ? "default" : "ghost"}
-          size="default"
-        >
-          Playlists
-        </Button>
-        {query && (
-          <h1 className="text-xl font-bold">
-            Resultados para: <span className="text-lime-500">{query}</span>
-          </h1>
-        )}
-      </div>
-      <ScrollArea className="h-full">
-        <div className="flex flex-wrap flex-1 h-full gap-6 p-6">
-          {result?.data?.map((item: SearchResultType) =>
-            type === "song" ? (
-              <SearchResultItem
-                key={item?.id}
-                result={item}
-                type={"song"}
-              />
-            ) : (
-              <SearchResultItem
-                key={item?.id}
-                result={item}
-                type={"playlist"}
-              />
-            )
+  return (
+    <div className="h-full flex flex-col pb-4">
+      <div className="w-full flex justify-between gap-2 p-4 border-b border-zinc-800">
+        <div className="flex gap-2">
+          <Button
+            onClick={() => setType("song")}
+            variant={type === "song" ? "default" : "ghost"}
+            size="default"
+          >
+            Músicas
+          </Button>
+
+          <Button
+            onClick={() => setType("playlist")}
+            variant={type === "playlist" ? "default" : "ghost"}
+            size="default"
+          >
+            Playlists
+          </Button>
+          {query && (
+            <>
+              <Separator orientation="vertical" />
+              <h1 className="text-xl font-bold">
+                Resultados para: <span className="text-lime-500">{query}</span>
+              </h1>
+            </>
           )}
         </div>
-      </ScrollArea>
+        <Pagination
+          currentPage={Number(page || 1)}
+          totalPages={result?.data?.totalPages || 1}
+          setPage={handlePageChange}
+        />
+      </div>
+      <div className="grid grid-cols-6 gap-7 mx-auto py-12">
+        {result?.data?.songs &&
+          result?.data?.songs?.map((item: SearchResultType) => (
+            <SearchResultItem key={item?.id} result={item} type={"song"} />
+          ))}
+        {result?.data?.playlists &&
+          result?.data?.playlists?.map((item: SearchResultType) => (
+            <SearchResultItem key={item?.id} result={item} type={"playlist"} />
+          ))}
+      </div>
     </div>
   );
 }
