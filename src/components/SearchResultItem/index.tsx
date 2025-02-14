458a015259528f9c0bebce7dae4a3b680cb98cb0
GabrielFrutuoso <mackbiel03@gmail.com>
2025-02-14 18:13:17 -0300

    feat: like and dislike playlists in search

--- src/components/SearchResultItem/index.tsx
+++ src/components/SearchResultItem/index.tsx
@@ -1,8 +1,11 @@
-import { Play } from "lucide-react";
+import { Heart, Play } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import { usePlaylistStore } from "@/store/playlistStore";
 import { SearchResultType } from "@/app/types/SearchResult.type";
+import { useLikePlaylist } from "@/hooks/requests/like/useLikePlaylist";
+import { useUnlikePlaylist } from "@/hooks/requests/like/useUnlikePlaylist";
+import { useUserStore } from "@/store/userStore";
 
 export const SearchResultItem = ({
   result,
@@ -13,6 +16,9 @@ export const SearchResultItem = ({
 }) => {
   const { singleSongId, playlistId, setPlaylistId, setSingleSongId, setIndex } =
     usePlaylistStore();
+  const { mutate: likePlaylist } = useLikePlaylist();
+  const { mutate: unlikePlaylist } = useUnlikePlaylist();
+  const { user } = useUserStore();
 
   const handleSongSelect = (id: string | number) => {
     if (type === "song") {
@@ -26,6 +32,20 @@ export const SearchResultItem = ({
     }
   };
 
+  const handleLikeUnlikePlaylist = () => {
+    if(user?.likedPlaylists?.some((likedPlaylist) => likedPlaylist.playlist.id === result.id)){
+      unlikePlaylist({
+        playlistId: result.id,
+        userId: user.id
+      });
+    } else {
+      likePlaylist({
+        playlistId: result.id,
+        userId: user?.id || ''
+      });
+    }
+  }
+
   return (
     <div
       className={`flex flex-col gap-2 w-full cursor-pointer group ${
@@ -36,12 +56,20 @@ export const SearchResultItem = ({
       }`}
     >
       <div className="relative w-full aspect-square">
-        <button
-          onClick={() => handleSongSelect(result.id)}
-          className="flex items-center justify-center bg-white/25 group-hover:visible invisible absolute w-full h-full rounded-lg z-10"
-        >
-          <Play className="w-8 h-8 sm:w-10 sm:h-10" />
-        </button>
+        <div className="flex items-center justify-center bg-white/15 group-hover:visible invisible absolute w-full h-full rounded-lg z-10">
+          <button
+            className="flex justify-center items-center w-8 h-8 sm:w-10 sm:h-10 p-1 rounded-lg absolute right-2 bottom-2 bg-transparent [&_svg]:size-10 text-secondary-foreground"
+            onClick={() => handleSongSelect(result.id)}
+          >
+            <Play />
+          </button>
+          <button
+            className="flex justify-center items-center w-8 h-8 sm:w-10 sm:h-10 p-1 rounded-lg bg-transparent [&_svg]:size-12 text-secondary-foreground"
+            onClick={() => handleLikeUnlikePlaylist()}
+          >
+            <Heart />
+          </button>
+        </div>
         <Image
           className="object-cover aspect-square rounded-lg"
           draggable={false}
