953c92848c01bd5f5ef53f3bdfe029152c240b5b
GabrielFrutuoso <mackbiel03@gmail.com>
2025-03-19 17:34:35 -0300

    feat: implement edit proffile hook

--- src/forms/EditProfileForm/index.tsx
+++ src/forms/EditProfileForm/index.tsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import { FormField } from '../FormField'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
+"use client";
+
+import React, { useState } from "react";
+import { FormField } from "../FormField";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useEditProfile } from "@/hooks/requests/user/useEditProfile";
+import { Button } from "@/components/ui/button";
+import { useUserStore } from "@/store/userStore";
+import { DialogFooter } from "@/components/ui/dialog";
+import { Pencil } from "lucide-react";
 
 export const EditProfileForm = () => {
+  const { user } = useUserStore();
+  const { mutate: editProfile } = useEditProfile();
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    editProfile({ userId: user?.id as string, username: username as string });
+  };
+
   return (
-    <form className="flex items-center gap-6">
-    <div className="flex flex-col items-center gap-6 flex-1">
+    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-6">
       <FormField>
         <Label htmlFor="username">username</Label>
-        <Input name="title" type="text" id="title" />
+        <Input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          name="username"
+          type="text"
+          id="username"
+        />
       </FormField>
-    </div>
-  </form>
-  )
-}
+      <DialogFooter className="w-full">
+        <Button className="w-full" type="submit">
+          <Pencil /> Salvar
+        </Button>
+      </DialogFooter>
+    </form>
+  );
+};
